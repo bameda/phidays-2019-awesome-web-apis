4044012e717ef72c48bc253786e4382ef3c9b49a
David Barragán Merino <david.barragan@kaleidos.net>
2019-06-11 21:48:47 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+yarn run build
 
 # navigate into the build output directory
 cd dist
@@ -12,9 +12,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-#git init
-#git add -A
-#git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 #git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
