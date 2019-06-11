b90dd5075cfcf91ee3f8ed007d98c03a93024bfb
David Barragán Merino <david.barragan@kaleidos.net>
2019-06-11 22:50:26 +0200

    Fix manifest

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/phidays-2019-awesome-web-apis/'
-    : '/'
+    : '/',
+  pwa: {
+    name: 'PhiDays 2019',
+    manifestPath: 'manifest.json',
+    themeColor: '#ff2d58',
+    msTileColor: '#000000'
+  }
 }
