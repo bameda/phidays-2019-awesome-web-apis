0d6c14404fa8bd76b063b6456e73ac6bfa699fe4
David Barragán Merino <david.barragan@kaleidos.net>
2019-06-14 00:04:27 +0200

    deploy

---
+++ precache-manifest.edacee982c8124d45eb9b7f0bd61a3f7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af05bf0d6a9e13c1154d",
+    "url": "/phidays-2019-awesome-web-apis/css/app.7ee29ec4.css"
+  },
+  {
+    "revision": "a4ef3ea86b7038d9c38d",
+    "url": "/phidays-2019-awesome-web-apis/css/chunk-vendors.81e18a23.css"
+  },
+  {
+    "revision": "34fa6e77cac2e5cd3126",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-.71e2f7da.css"
+  },
+  {
+    "revision": "82cc1bb899781f137a69",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-animation.7a5e6037.css"
+  },
+  {
+    "revision": "84a0c2da55a3f91f95bb",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-audio.d9ea0ce8.css"
+  },
+  {
+    "revision": "c80737e369ce0a096cdd",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-battery.5ac38ce4.css"
+  },
+  {
+    "revision": "c5225b31c67a8b3c02f8",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-geolocation.267ae566.css"
+  },
+  {
+    "revision": "ee56e756243eef510880",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-media-capture-and-stream.f7ee367b.css"
+  },
+  {
+    "revision": "b293deaa0accc2e2ce45",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-notification.081750cb.css"
+  },
+  {
+    "revision": "14bb05c9d12f21912437",
+    "url": "/phidays-2019-awesome-web-apis/css/demo-vibration.b5c94d7f.css"
+  },
+  {
+    "revision": "4c78d186070bcdaedf7e1862ce9f9ac8",
+    "url": "/phidays-2019-awesome-web-apis/img/logo-letters.4c78d186.png"
+  },
+  {
+    "revision": "8fc4a7d5a13c8bdf90101795ff8ba972",
+    "url": "/phidays-2019-awesome-web-apis/img/logo.8fc4a7d5.png"
+  },
+  {
+    "revision": "6b4bf6c0e290ec2a40f8986a87569f0c",
+    "url": "/phidays-2019-awesome-web-apis/index.html"
+  },
+  {
+    "revision": "af05bf0d6a9e13c1154d",
+    "url": "/phidays-2019-awesome-web-apis/js/app.66ac6881.js"
+  },
+  {
+    "revision": "a4ef3ea86b7038d9c38d",
+    "url": "/phidays-2019-awesome-web-apis/js/chunk-vendors.5abfba91.js"
+  },
+  {
+    "revision": "34fa6e77cac2e5cd3126",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-.2341b24d.js"
+  },
+  {
+    "revision": "82cc1bb899781f137a69",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-animation.abd504ef.js"
+  },
+  {
+    "revision": "84a0c2da55a3f91f95bb",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-audio.b42bdf44.js"
+  },
+  {
+    "revision": "c80737e369ce0a096cdd",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-battery.36a3d88d.js"
+  },
+  {
+    "revision": "c5225b31c67a8b3c02f8",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-geolocation.819c759a.js"
+  },
+  {
+    "revision": "ee56e756243eef510880",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-media-capture-and-stream.5785cd95.js"
+  },
+  {
+    "revision": "b293deaa0accc2e2ce45",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-notification.cdffcd53.js"
+  },
+  {
+    "revision": "14bb05c9d12f21912437",
+    "url": "/phidays-2019-awesome-web-apis/js/demo-vibration.4b539872.js"
+  },
+  {
+    "revision": "db0c3bdea4efa7920913cccd90a7362f",
+    "url": "/phidays-2019-awesome-web-apis/manifest.json"
+  },
+  {
+    "revision": "167cb3ae75c1379a4108c9e3f4326ac3",
+    "url": "/phidays-2019-awesome-web-apis/media/tere_llio_parda.167cb3ae.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/phidays-2019-awesome-web-apis/robots.txt"
+  }
+]);
